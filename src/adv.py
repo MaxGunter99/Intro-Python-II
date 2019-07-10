08829012ddf5476074c784a8585dfe742937dfb8
maxgunter99 <maxgunter99@gmail.com>
2019-07-10 16:34:22 -0500

    making progress

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,18 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."""),
 }
 
 
@@ -33,18 +27,35 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
-#
 # Main
-#
-
 # Make a new player object that is currently in the 'outside' room.
+name = input( "What is your name, Adventurer?\n" )
+player_one = Player( name , room[ "outside" ] )
+quest_started = True
+print( "The Adventure Begins:    ☁️  🌖  ☁️\n" )
+print( "__🁢_________________________⎧⎫‾‾‾‾‾‾" )
 
 # Write a loop that:
 #
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
-#
+
+while quest_started:
+
+    #LOCATION
+    adventurer_name = player_one.name
+    current_room_name = player_one.current_room.name
+    current_room_description = player_one.current_room.description
+
+    print( "\n" , "Adventurer:" , adventurer_name , "\n" , "Location:" , current_room_name , "\n" , "Description:" , current_room_description , "\n" )
+    decision = input( "Where do you want to go?\n" )
+    if decision == "n":
+        print( room['outside'].n_to )
+    else:
+        print( "You can not go there ... " )
+    quest_started = False
+
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
 #
--- src/player.py
+++ src/player.py
@@ -1,2 +1,38 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+# PLAYER INITIAL STATE
+class Player:
+    def __init__( self , name , current_room , backpack_equipped = False , items = [] ):
+        self.name = name
+        self.current_room = current_room
+        self.backpack_equipped = backpack_equipped
+        self.items = items
+
+    # TAKE ITEM
+    def take_item( self , item ):
+
+        # EQUIP BACKPACK
+        if item.name == "Backpack":
+            self.backpack_equipped = True
+
+        # MAKE USE OF BACKPACK
+        if len( self.items ) <= 2 or self.backpack_equipped == True:
+            self.items += item
+            return True
+        else:
+            return False
+    
+    # REMOVE ITEM
+    def remove_item( self , item ):
+
+        # TAKE OFF BACKPACK
+        if item.name == "Backpack":
+            self.backpack_equipped = False
+        
+        # REMOVE ITEM
+        for i in self.items:
+            if i.name == item.name:
+                self.items.remove( item )
+
+        
